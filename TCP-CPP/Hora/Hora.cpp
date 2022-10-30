1b26fb034e7c8bb882b5d43fbcc70890b2807d7d
Juan <juanbianchi@icloud.com>
2022-10-29 22:57:05 -0300

    Se agrega clase Hora

---
+++ TCP-CPP/Hora/Hora.cpp
@@ -0,0 +1,116 @@
+#include "Hora.h"
+
+Hora::Hora()
+{
+    this->segRel = 1;
+}
+
+
+
+Hora::Hora(int seg, int minut, int horas)
+{
+    setHoraRelativa(seg, minut, horas);
+}
+
+
+Hora::Hora(Hora& hor)
+{
+    this->segRel = hor.segRel;
+}
+
+
+
+void Hora::setHoraRelativa(int seg, int minut, int horas)
+{
+    this->segRel = horas * 3600 + minut * 60 + seg;
+
+}
+
+
+void Hora::getHoraRelativa(int& seg, int& minut, int& horas)
+{
+    int minutosTotales = this->segRel / 60;
+    minut = minutosTotales % 60;
+    seg = this->segRel % 60;
+    horas = minutosTotales / 60;
+}
+
+
+
+Hora Hora::operator  ++(int)
+{
+    Hora hor(*this);
+
+    this->segRel ++;
+
+    return hor;
+}
+
+
+
+Hora& Hora::operator --()
+{
+    -- this->segRel;
+
+    return (*this);
+}
+
+
+
+Hora Hora::operator --(int)
+{
+    Hora hor(*this);
+
+    this->segRel --;
+
+    return hor;
+}
+
+
+
+Hora& Hora::operator += (int seg)
+{
+    this->segRel += seg;
+
+    return *this;
+}
+
+
+
+Hora& Hora::operator += (Hora& otraHora)
+{
+    this->segRel += otraHora.segRel;
+
+    return *this;
+}
+
+
+
+Hora& Hora::operator -= (int seg)
+{
+    this->segRel -= seg;
+
+    return *this;
+}
+
+
+
+Hora& Hora::operator -= (Hora& otraHora)
+{
+    this->segRel -= otraHora.segRel;
+
+    return *this;
+}
+
+
+
+ostream& operator << (ostream& os, Hora& hora)
+{
+    int hor, minuto, segundo;
+
+    hora.getHoraRelativa(segundo, minuto, hor);
+
+    os << hora << " : " << minuto << " : " << segundo << endl;
+
+    return os;
+}
---
+++ TCP-CPP/Hora/Hora.h
@@ -0,0 +1,40 @@
+#ifndef HORA_H
+#define HORA_H
+
+#include <iostream>
+
+using namespace std;
+
+class Hora
+{
+    public:
+        Hora();
+        Hora(int seg, int minut, int horas);
+        Hora(Hora& hor);
+
+
+        void setHoraRelativa(int seg, int minut, int horas);
+        void getHoraRelativa(int& seg, int& minut, int& horas);
+
+        inline Hora& operator ++(){++ this->segRel; return *this;};
+        Hora operator  ++(int);
+        Hora& operator --();
+        Hora operator --(int);
+        Hora& operator += (int seg);
+        Hora& operator += (Hora& otraHora);
+        Hora& operator -= (int seg);
+        Hora& operator -= (Hora& otraHora);
+        inline bool operator == (Hora& otraHora)const{return this->segRel == otraHora.segRel;};
+        inline bool operator != (Hora& otraHora)const{return this->segRel != otraHora.segRel;};
+        inline bool operator < (Hora& otraHora)const{return this->segRel < otraHora.segRel;};
+        inline bool operator > (Hora& otraHora)const{return this->segRel > otraHora.segRel;};
+        inline bool operator <= (Hora& otraHora)const{return this->segRel <= otraHora.segRel;};
+        inline bool operator >= (Hora& otraHora)const{return this->segRel >= otraHora.segRel;};
+
+        friend ostream& operator << (ostream& os, Hora& hora);
+
+    private:
+        int segRel;
+};
+
+#endif // HORA_H
